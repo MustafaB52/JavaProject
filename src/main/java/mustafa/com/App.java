06e5b2d2299bf677206fb41d9151395f2c717ab1
Mustafa B <Mustafa_B@infosys.com>
2016-11-16 15:21:08 +0530

    New project

---
+++ src/main/java/mustafa/com/App.java
@@ -0,0 +1,16 @@
+package mustafa.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private App() {
+	
+	}
+	public static void main( String[] args )
+    {
+        logger.log( "Hello World!" );
+    }
+}
