22eeabf1137680e54851ecd417deed4af9bac82f
Mustafa B <Mustafa_B@infosys.com>
2016-11-17 11:23:57 +0530

    Updated source file once AGAIN

--- src/main/java/mustafa/com/App.java
+++ src/main/java/mustafa/com/App.java
@@ -1,14 +1,12 @@
 package mustafa.com;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class App 
 {
 	private final static Logger logger = Logger.getLogger(App.class.getName());
-	private App()
+	App()
 	{
 	
 	}
