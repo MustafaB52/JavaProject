f80d1d66e5db2c23b8d80698ef08340c24edb1b6
Mustafa B <Mustafa_B@infosys.com>
2016-11-17 11:10:33 +0530

    Reverted comments in source file

--- src/main/java/mustafa/com/App.java
+++ src/main/java/mustafa/com/App.java
@@ -2,6 +2,13 @@ package mustafa.com;
 
 import java.util.logging.Logger;
 
+// assumes the current class is called MyLogger
+//private final static Logger LOGGER = Logger.getLogger(MyLogger.class.getName());
+
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
 	private App()
